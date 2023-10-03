97d1ff09531c8563acc97aa62d3060af414c1899
Hosna Qasmei <hosna.qasmei@gmail.com>
2023-10-02 22:36:40 -0700

    Updated reading

---
+++ src/components/react/Reading.tsx
@@ -0,0 +1,44 @@
+import { READING } from '../../consts'
+
+const Reading = () => {
+  const groupedBooks = READING.reduce((acc, book) => {
+    const year = book.dateFinished.split('-')[0]
+    if (!acc[year]) {
+      acc[year] = []
+    }
+    acc[year].push(book)
+    return acc
+  }, {})
+
+  const sortedYears = Object.keys(groupedBooks).sort(
+    (a, b) => parseInt(b) - parseInt(a)
+  )
+
+  return (
+    <div className='grid grid-cols-1 gap-10 text-zinc-200 pb-10'>
+      {sortedYears.map((year, idx) => {
+        const booksForYear = groupedBooks[year].sort((a, b) =>
+          b.dateFinished.localeCompare(a.dateFinished)
+        )
+        return (
+          <div key={idx}>
+            <h2 className='font-bold text-xl mb-2'>{year}</h2>
+            {booksForYear.map((book, idx) => (
+              <div
+                key={idx}
+                className='grid  grid-cols-1 sm:grid-cols-4 sm:gap-4 text-sm py-1 text-zinc-300'
+              >
+                <span className='font-semibold'>{book.title}</span>
+                <p>{book.author}</p>
+                <p>{book.dateFinished}</p>
+                <p>{'★'.repeat(book.rating)}</p>
+              </div>
+            ))}
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
+export default Reading
--- src/consts.ts
+++ src/consts.ts
@@ -5,8 +5,6 @@
 export const SITE_TITLE = 'Hosna Qasmei'
 export const SITE_DESCRIPTION = 'Welcome to my website!'
 export const LOCATION = 'Los Angeles, CA'
-export const BIO =
-  'I am a Software Developer building software to solve problems. Here is my bio of things I should talk about and say and what not. I will replace this with more stuff soon.'
 
 // Projects
 export const PROJECTS = [
@@ -68,3 +66,547 @@ export const GITHUB = 'https://github.com/hqasmei'
 export const YOUTUBE = 'https://www.youtube.com/@hqasmei'
 export const DISCORD = 'https://discord.com/invite/agzuPEVxhT'
 export const LINKEDIN = 'https://www.linkedin.com/in/hosnaqasmei/'
+
+// Reading
+export const READING = [
+  {
+    title: 'Exhalation',
+    author: 'Ted Chiang',
+    dateFinished: '2023-01-05',
+    rating: 4
+  },
+  {
+    title: 'The Road Less Traveled, 25th Anniversary Edition',
+    author: 'M. Scott Peck',
+    dateFinished: '2023-01-22',
+    rating: 4
+  },
+  {
+    title: 'The Mountain Is You',
+    author: 'Brianna Wiest',
+    dateFinished: '2023-01-30',
+    rating: 3
+  },
+  {
+    title: 'The Wisdom of a Broken Heart',
+    author: 'Susan Piver',
+    dateFinished: '2023-02-01',
+    rating: 3
+  },
+  {
+    title: 'Building a Story Brand',
+    author: 'Donald Miller',
+    dateFinished: '2022-12-14',
+    rating: 4
+  },
+  {
+    title: 'Rising Strong ',
+    author: 'Brene Brown',
+    dateFinished: '2022-12-13',
+    rating: 5
+  },
+  {
+    title: 'The 100 Year Life',
+    author: 'Lynda Gratton and Andrew Scott',
+    dateFinished: '2022-11-01',
+    rating: 3
+  },
+  {
+    title: 'Aristotle and Dante Dive Into the Waters of the World',
+    author: 'Benjamin Alire Sáenz',
+    dateFinished: '2022-10-20',
+    rating: 4
+  },
+  {
+    title: 'Atlas of the Heart',
+    author: 'Brené Brown',
+    dateFinished: '2022-09-30',
+    rating: 5
+  },
+  {
+    title: 'Attached',
+    author: 'Amir Levine, Rachel Heller',
+    dateFinished: '2022-08-19',
+    rating: 4
+  },
+  {
+    title: 'Broken Horses',
+    author: 'Brandi Carlile',
+    dateFinished: '2022-07-29',
+    rating: 4
+  },
+  {
+    title: 'Dare to Lead',
+    author: 'Brené Brown',
+    dateFinished: '2022-07-20',
+    rating: 5
+  },
+  {
+    title: 'Daring Greatly',
+    author: 'Brené Brown',
+    dateFinished: '2022-07-15',
+    rating: 4
+  },
+  {
+    title: 'Emotional Agility',
+    author: 'Susan David',
+    dateFinished: '2022-07-10',
+    rating: 4
+  },
+  {
+    title: 'How to Do the Work',
+    author: 'Nicole LePera',
+    dateFinished: '2022-07-05',
+    rating: 4
+  },
+  {
+    title: 'In the Dream House',
+    author: 'Carmen Maria Machado',
+    dateFinished: '2022-07-01',
+    rating: 4
+  },
+  {
+    title: 'Play Well with Others',
+    author: 'Eric Barker',
+    dateFinished: '2022-06-28',
+    rating: 5
+  },
+  {
+    title: 'Permission to Feel',
+    author: 'Ph.D. Marc Brackett',
+    dateFinished: '2022-06-23',
+    rating: 4
+  },
+  {
+    title: 'Quiet',
+    author: 'Susan Cain',
+    dateFinished: '2022-06-18',
+    rating: 5
+  },
+  {
+    title: 'The Body Keeps the Score',
+    author: 'Bessel A. Van der Kolk',
+    dateFinished: '2022-06-15',
+    rating: 5
+  },
+  {
+    title: 'The Gifts of Imperfection',
+    author: 'Brené Brown',
+    dateFinished: '2022-06-10',
+    rating: 5
+  },
+  {
+    title: 'The Last Black Unicorn',
+    author: 'Tiffany Haddish',
+    dateFinished: '2022-06-05',
+    rating: 4
+  },
+  {
+    title: 'The Stranger',
+    author: 'Albert Camus',
+    dateFinished: '2022-06-01',
+    rating: 4
+  },
+  {
+    title: 'The Way of Integrity',
+    author: 'Martha Beck',
+    dateFinished: '2022-05-28',
+    rating: 4
+  },
+  {
+    title: "Why Won't You Apologize?",
+    author: 'Harriet Lerner',
+    dateFinished: '2022-05-23',
+    rating: 5
+  },
+  {
+    title: 'WOLFPACK',
+    author: 'Abby Wambach',
+    dateFinished: '2022-05-18',
+    rating: 5
+  },
+  {
+    title: 'How High We Go in the Dark',
+    author: 'Sequoia Nagamatsu',
+    dateFinished: '2022-07-17',
+    rating: 5
+  },
+  {
+    title: 'What Happened to You?',
+    author: 'Oprah Winfrey and Bruce D. Perry',
+    dateFinished: '2022-07-25',
+    rating: 4
+  },
+  {
+    title: 'Start with Why',
+    author: 'Simon Sinek',
+    dateFinished: '2022-07-31',
+    rating: 5
+  },
+  {
+    title: 'The Strength in Our Scars',
+    author: 'Bianca Sparacino',
+    dateFinished: '2022-08-05',
+    rating: 3
+  },
+  {
+    title: 'Forward',
+    author: 'Abby Wambach',
+    dateFinished: '2022-08-06',
+    rating: 4
+  },
+  {
+    title: 'Anything You Want',
+    author: 'Derek Sivers',
+    dateFinished: '2022-08-07',
+    rating: 4
+  },
+  {
+    title: 'Love is a Dog From Hell',
+    author: 'Charles Bukowski',
+    dateFinished: '2022-08-08',
+    rating: 3
+  },
+  {
+    title: 'The Power of Regret',
+    author: 'Daniel H. Pink',
+    dateFinished: '2022-08-12',
+    rating: 3
+  },
+  {
+    title: 'The Lean Startup',
+    author: 'Eric Ries',
+    dateFinished: '2022-08-14',
+    rating: 5
+  },
+  {
+    title: 'Bittersweet',
+    author: 'Susan Cain',
+    dateFinished: '2022-08-14',
+    rating: 4
+  },
+  {
+    title: 'Barking Up the Wrong Tree',
+    author: 'Eric Barker',
+    dateFinished: '2022-08-14',
+    rating: 5
+  },
+  {
+    title: 'Atomic Habits',
+    author: 'James Clear',
+    dateFinished: '2022-08-14',
+    rating: 4
+  },
+  {
+    title: 'Self-Compassion',
+    author: 'Dr. Kristin Neff',
+    dateFinished: '2022-08-14',
+    rating: 5
+  },
+  {
+    title: 'Zero to One',
+    author: 'Blake Masters and Peter Thiel',
+    dateFinished: '2022-08-18',
+    rating: 3
+  },
+  {
+    title: 'Come as You Are',
+    author: 'Emily Nagoski',
+    dateFinished: '2022-08-22',
+    rating: 4
+  },
+  {
+    title: 'Love Poems',
+    author: 'Pablo Neruda',
+    dateFinished: '2022-08-25',
+    rating: 5
+  },
+  {
+    title: '101 Essays That Will Change the Way You Think',
+    author: 'Brianna Wiest',
+    dateFinished: '2022-08-26',
+    rating: 4
+  },
+  {
+    title: 'Set Boundaries, Find Peace',
+    author: 'Nedra Glover Tawwab',
+    dateFinished: '2022-08-31',
+    rating: 4
+  },
+  {
+    title: 'Grit',
+    author: 'Angela Duckworth',
+    dateFinished: '2022-09-04',
+    rating: 3
+  },
+  {
+    title: 'Designing Your Life',
+    author: 'Bill Burnett and Dave Evans',
+    dateFinished: '2022-09-05',
+    rating: 3
+  },
+  {
+    title: 'Crying in H Mart',
+    author: 'Michelle Zauner',
+    dateFinished: '2022-09-14',
+    rating: 4
+  },
+  {
+    title: 'Girls Can Kiss Now',
+    author: 'Jill Gutowitz',
+    dateFinished: '2022-09-19',
+    rating: 3
+  },
+  {
+    title: 'Rich Dad, Poor Dad',
+    author: 'Robert T. Kiyosaki',
+    dateFinished: '2022-10-07',
+    rating: 3
+  },
+  {
+    title: 'Breath',
+    author: 'James Nestor ',
+    dateFinished: '2022-10-12',
+    rating: 4
+  },
+  {
+    title: 'Think and Grow Rich',
+    author: 'Napoleon Hill',
+    dateFinished: '2022-10-23',
+    rating: 4
+  },
+  {
+    title: "You're Not Listening",
+    author: 'Kate Murphy',
+    dateFinished: '2022-11-10',
+    rating: 4
+  },
+  {
+    title: 'Think Again',
+    author: 'Adam Grant',
+    dateFinished: '2022-11-28',
+    rating: 5
+  },
+  {
+    title: 'All About Love',
+    author: 'bell hooks',
+    dateFinished: '2022-12-14',
+    rating: 5
+  },
+  {
+    title: 'Codependent No More',
+    author: 'Melody Beattie',
+    dateFinished: '2022-12-14',
+    rating: 3
+  },
+  {
+    title: 'Darius the Great Is Not Okay',
+    author: 'Adib Khorram',
+    dateFinished: '2022-12-13',
+    rating: 3
+  },
+  {
+    title: 'Untamed',
+    author: 'Glennon Doyle',
+    dateFinished: '2022-11-01',
+    rating: 4
+  },
+  {
+    title: 'Goodbye, Again',
+    author: 'Jonny Sun',
+    dateFinished: '2022-10-20',
+    rating: 5
+  },
+  {
+    title: 'How to Win Friends and Influence People',
+    author: 'Dale Carnegie',
+    dateFinished: '2022-09-30',
+    rating: 5
+  },
+  {
+    title: 'Maybe You Should Talk to Someone',
+    author: 'Lori Gottlieb',
+    dateFinished: '2022-08-19',
+    rating: 5
+  },
+  {
+    title: 'Milk and Honey',
+    author: 'Rupi Kaur',
+    dateFinished: '2022-07-29',
+    rating: 3
+  },
+  {
+    title: 'The 7 Habits of Highly Effective People',
+    author: 'Stephen R. Covey',
+    dateFinished: '2022-06-17',
+    rating: 5
+  },
+  {
+    title: 'The Alchemist',
+    author: 'Paulo Coelho',
+    dateFinished: '2022-05-27',
+    rating: 4
+  },
+  {
+    title: 'The Art of Happiness',
+    author: 'Dalai Lama ',
+    dateFinished: '2022-04-15',
+    rating: 3
+  },
+  {
+    title: 'The Four Agreements',
+    author: 'Don Miguel Ruiz',
+    dateFinished: '2022-03-25',
+    rating: 5
+  },
+  {
+    title: 'Tuesdays with Morrie',
+    author: 'Mitch Albom',
+    dateFinished: '2022-02-11',
+    rating: 5
+  },
+  {
+    title: 'Unfu*k Yourself',
+    author: 'Gary John Bishop',
+    dateFinished: '2022-01-21',
+    rating: 2
+  },
+  {
+    title: 'Unlearn: 101 Simple Truths for a Better Life',
+    author: 'Humble the Poet',
+    dateFinished: '2021-12-31',
+    rating: 4
+  },
+  {
+    title: 'What Would Frida Do?',
+    author: 'Arianna Davis',
+    dateFinished: '2021-11-19',
+    rating: 3
+  },
+  {
+    title: 'Who Moved My Cheese?',
+    author: 'Spencer Johnson',
+    dateFinished: '2021-10-29',
+    rating: 4
+  },
+  {
+    title: 'You Are a Badass',
+    author: 'Jen Sincero',
+    dateFinished: '2021-09-17',
+    rating: 2
+  },
+  {
+    title: 'Flowers for Algernon',
+    author: 'Daniel Keyes',
+    dateFinished: '2021-08-27',
+    rating: 5
+  },
+  {
+    title: 'Secrets of Divine Love',
+    author: 'A. Helwa',
+    dateFinished: '2021-07-15',
+    rating: 4
+  },
+  {
+    title: 'A Dutiful Boy',
+    author: 'Mohsin Zaidi',
+    dateFinished: '2021-06-25',
+    rating: 4
+  },
+  {
+    title: 'The Prophet',
+    author: 'Kahlil Gibran',
+    dateFinished: '2021-05-13',
+    rating: 5
+  },
+  {
+    title: 'The Complete Poetry',
+    author: 'Maya Angelou',
+    dateFinished: '2021-04-23',
+    rating: 5
+  },
+  {
+    title: 'The House in the Cerulean Sea',
+    author: 'TJ Klune',
+    dateFinished: '2021-03-11',
+    rating: 4
+  },
+  {
+    title: 'All the Light We Cannot See',
+    author: 'Anthony Doerr',
+    dateFinished: '2021-02-21',
+    rating: 5
+  },
+  {
+    title: 'Dark Matter',
+    author: 'Blake Crouch',
+    dateFinished: '2021-01-09',
+    rating: 4
+  },
+  {
+    title: 'Recursion',
+    author: 'Blake Crouch',
+    dateFinished: '2020-12-19',
+    rating: 4
+  },
+  {
+    title: 'A Little Life',
+    author: 'Hanya Yanagihara',
+    dateFinished: '2020-11-07',
+    rating: 4
+  },
+  {
+    title: 'What I Talk about when I Talk about Running',
+    author: 'Haruki Murakami',
+    dateFinished: '2020-10-17',
+    rating: 4
+  },
+  {
+    title: 'The Silent Patient',
+    author: 'Alex Michaelides',
+    dateFinished: '2020-09-05',
+    rating: 3
+  },
+  {
+    title: 'The Midnight Library',
+    author: 'Matt Haig',
+    dateFinished: '2020-08-15',
+    rating: 5
+  },
+  {
+    title: 'Born a Crime',
+    author: 'Trevor Noah',
+    dateFinished: '2020-07-25',
+    rating: 5
+  },
+  {
+    title: 'Sum',
+    author: 'David Eagleman',
+    dateFinished: '2020-06-12',
+    rating: 4
+  },
+  {
+    title: 'The Untethered Soul (EasyRead Edition)',
+    author: 'Michael A. Singer',
+    dateFinished: '2020-05-22',
+    rating: 3
+  },
+  {
+    title: 'The Hunger Games',
+    author: 'Suzanne Collins',
+    dateFinished: '2020-04-10',
+    rating: 4
+  },
+  {
+    title: 'A New Earth',
+    author: 'Eckhart Tolle',
+    dateFinished: '2020-03-20',
+    rating: 5
+  },
+  {
+    title: 'Heart of Darkness',
+    author: 'Joseph Conrad',
+    dateFinished: '2020-02-05',
+    rating: 1
+  }
+]
