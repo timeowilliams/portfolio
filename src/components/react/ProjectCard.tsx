881e914e6d3eb39e391239603e9fec41f8cc7532
Hosna Qasmei <hosna.qasmei@gmail.com>
2023-10-02 23:26:55 -0700

    Added back icon for project

--- src/components/react/ProjectCard.tsx
+++ src/components/react/ProjectCard.tsx
@@ -9,12 +9,15 @@ type Props = {
 const ProjectCard = (props: Props) => {
   const { title, description, link, image, status } = props
   return (
-    <a href={link} target='_blank' className='flex flex-col space-y-1'>
-      <p className='underline underline-offset-4 decoration-zinc-600 hover:decoration-zinc-100 duration-150 hover:text-zinc-100 text-zinc-200'>
-        {title}
-      </p>
-      <p className='text-zinc-400'>{description}</p>
-    </a>
+    <div className='flex flex-col space-y-2'>
+      <img src={image} alt='' class='h-6 w-6 shadow-lg' />
+      <div class='flex flex-col'>
+        <a href={link} target='_blank' className='underline underline-offset-4 decoration-zinc-600 hover:decoration-zinc-100 duration-150 hover:text-zinc-100 text-zinc-200'>
+          {title}
+        </a>
+        <p className='text-zinc-400'>{description}</p>
+      </div>
+    </div>
   )
 }
 
