4e90bbb1c86aa15f3ae1f1d0e809d852b86039ee
Hosna Qasmei <hosna.qasmei@gmail.com>
2023-10-08 13:38:24 -0700

    One more time

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,14 @@
-import { defineConfig } from 'astro/config'
-import sitemap from '@astrojs/sitemap'
-import tailwind from '@astrojs/tailwind'
-import react from '@astrojs/react'
-import vercel from '@astrojs/vercel/serverless'
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
+import vercel from '@astrojs/vercel/serverless';
+
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://hosnaqasmei.com',
   integrations: [sitemap(), tailwind(), react()],
   output: 'server',
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true
-    }
-  })
-})
+  adapter: vercel()
+});
