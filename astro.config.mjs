d878c4b217eafcccce000b3a736b497e2cbcdec9
Hosna Qasmei <hosna.qasmei@gmail.com>
2023-10-08 13:29:49 -0700

    Added vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
-import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
-import tailwind from '@astrojs/tailwind';
-import react from '@astrojs/react';
-
+import { defineConfig } from 'astro/config'
+import sitemap from '@astrojs/sitemap'
+import tailwind from '@astrojs/tailwind'
+import react from '@astrojs/react'
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://hosnaqasmei.com',
-  integrations: [sitemap(), tailwind(), react()]
-});
+  integrations: [sitemap(), tailwind(), react()],
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true
+    }
+  })
+})
