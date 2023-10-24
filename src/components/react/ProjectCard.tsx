dda90448d44aa3688a45eb3b94c95fa6763b4b04
Hosna Qasmei <hosna.qasmei@gmail.com>
2023-10-23 23:52:23 -0700

    Updated projects

--- src/components/react/ProjectCard.tsx
+++ src/components/react/ProjectCard.tsx
@@ -20,23 +20,23 @@ const ProjectCard = (props: Props) => {
     <a
       href={link}
       target='_blank'
-      className='flex flex-row  space-x-2  group items-end'
+      className='flex flex-col  space-y-2  group items-start'
     >
-      <div className='flex flex-col space-y-1 max-w-sm'>
+      <div className='flex flex-row space-x-2 items-end'>
+        <span className='inline-flex items-center justify-center flex-shrink-0 rounded z-10 relative h-5 w-5 text-base'>
+          <img
+            src={image}
+            alt={title}
+            className='rounded z-10 relative h-5 w-5 text-base'
+          />
+        </span>
         <div className='underline underline-offset-4 decoration-zinc-600 group-hover:decoration-zinc-50 duration-150 group-hover:text-zinc-50 text-zinc-200'>
           {title}
         </div>
-
-        <p className='text-zinc-400'>{description}</p>
       </div>
-      <div className='flex-1 border-b border-dashed border-zinc-400'></div>
-      <span className='inline-flex items-center justify-center flex-shrink-0 rounded z-10 relative h-10 w-10 text-base'>
-        <img
-          src={image}
-          alt={title}
-          className='rounded z-10 relative h-10 w-10 text-base'
-        />
-      </span>
+      <div>
+        <p className='text-zinc-400 text-sm'>{description}</p>
+      </div>
     </a>
   )
 }
