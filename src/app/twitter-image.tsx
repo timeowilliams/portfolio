50ed9d8171b17aa917b252ffb98c9b24b4ca720c
Hosna Qasmei <hosna.qasmei@gmail.com>
2024-02-10 18:03:14 -0800

    testing og again

--- src/app/twitter-image.tsx
+++ src/app/twitter-image.tsx
@@ -3,52 +3,41 @@ import { ImageResponse } from 'next/og';
 // Route segment config
 export const runtime = 'edge';
 
-// Image metadata
-export const alt = 'About Acme';
-export const size = {
-  width: 1200,
-  height: 630,
-};
-
-export const contentType = 'image/png';
-
 // Image generation
 export default async function Image() {
-  // Font
-  const interSemiBold = fetch(
-    new URL('../../public/fonts/Inter-SemiBold.otf', import.meta.url),
-  ).then((res) => res.arrayBuffer());
-
   return new ImageResponse(
     (
-      // ImageResponse JSX element
       <div
         style={{
-          fontSize: 128,
-          background: 'white',
-          width: '100%',
           height: '100%',
+          width: '100%',
           display: 'flex',
-          alignItems: 'center',
+          flexDirection: 'column',
+          alignItems: 'flex-start',
           justifyContent: 'center',
+          backgroundImage: 'url(https://hosnaqasmei.com/og-bg.png)',
         }}
       >
-        About Acme
+        <div
+          style={{
+            marginLeft: 100,
+            marginRight: 100,
+            display: 'flex',
+            fontSize: 70,
+            letterSpacing: '-0.05em',
+            fontStyle: 'normal',
+            color: 'white',
+            lineHeight: '70px',
+            whiteSpace: 'pre-wrap',
+          }}
+        >
+          Hosna Qasmei
+        </div>
       </div>
     ),
-    // ImageResponse options
     {
-      // For convenience, we can re-use the exported opengraph-image
-      // size config to also set the ImageResponse's width and height.
-      ...size,
-      fonts: [
-        {
-          name: 'Inter',
-          data: await interSemiBold,
-          style: 'normal',
-          weight: 400,
-        },
-      ],
+      width: 1200,
+      height: 630,
     },
   );
 }
