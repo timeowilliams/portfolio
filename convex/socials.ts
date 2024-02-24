58539c4d6ef5474cba8f6d95beda12fdffe6dbe0
Hosna Qasmei <hosna.qasmei@gmail.com>
2024-02-23 19:07:09 -0800

    Added createRow to convex

--- convex/socials.ts
+++ convex/socials.ts
@@ -1,8 +1,23 @@
-import { query } from './_generated/server';
+import { v } from 'convex/values';
+
+import { mutation, query } from './_generated/server';
 
 export const get = query({
   args: {},
   handler: async (ctx) => {
     return await ctx.db.query('socials').collect();
+  },
+});
+
+export const createRow = mutation({
+  args: {
+    followersCount: v.string(),
+    platformName: v.string(),
+  },
+  handler: async (ctx, args) => {
+    await ctx.db.insert('socials', {
+      followersCount: args.followersCount,
+      platformName: args.platformName,
+    });
   },
 });
